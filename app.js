3ef402b1eb800178afc83aa494aa59ca1c3a29de
k-yanase <pyanase@gmail.com>
2026-09-13 10:54:14 +0900

    fix url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const GAS_URL = "YOUR_GAS_WEB_APP_URL_HERE";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbymzundW-n2WlYGyZWAOK19lFLA8-8ssrMu_HG1tk7IXk-OJnH0GYlM0Vcx2QbGjl2q/exec";
 
 let appState = {
     password: localStorage.getItem('mahjong_pwd') || '',
